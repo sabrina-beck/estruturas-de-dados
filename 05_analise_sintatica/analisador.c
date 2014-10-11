34b75bd26b06097b7731c16f3ebb5fd819f18411
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2014-10-11 08:47:23 -0300

    Lab 05: implementou tudo

--- 05_analise_sintatica/analisador.c
+++ 05_analise_sintatica/analisador.c
@@ -38,6 +38,9 @@ Erro Expressao();
 Erro Termo();
 Erro Fator();
 Erro Primario();
+Erro Potencia();
+void EliminaBrancos();
+boolean ehCaracterValido(char c);
 
 /* Função auxiliar */
 Erro montaErro(int codigo, int pos);
@@ -56,7 +59,19 @@ Erro InPos(char *infixa, char *posfixa) {
   indIn = 0;
   pos = posfixa;
   indPos = 0;
+  
+  /* Verifica cadeia vazia */
+  EliminaBrancos();
+  if(!in[indIn])
+    return montaErro(CADEIA_DE_BRANCOS, indPos);
+  
   erro = Expressao();
+
+  if(erro.codigoErro != EXPR_VALIDA)
+    return erro;
+  
+  if(in[indIn])
+      return montaErro(OPERADOR_ESPERADO, indIn);
   
   posfixa[indPos] = '\0';
   return erro;
@@ -81,73 +96,195 @@ Erro montaErro(int codigo, int posicao) {
 
 Erro Expressao() {
 /* Processa uma expressão da cadeia de entrada.  */
+  Erro erro;
 
-  Termo();
-  boolean fim = false;
-  do {
-    char atual = in[indIn];
-    if(atual == '+' || atual == '-') {
-      indIn++;
-      Termo();
-      pos[indPos++] = atual;
-    } else fim = true;
-  } while(!fim);
+  /* Se a expressão começar com um operador '+' ou '-', ele é unário */
+  if(in[indIn] == '+' || in[indIn] == '-')
+    erro = Primario();
+  else
+    erro = Termo();
+
+  if(erro.codigoErro != EXPR_VALIDA)
+    return erro;
+    
+  if(in[indIn]) {
+    boolean fim = false;
+    do {
+      char atual = in[indIn];
+      
+      if(!ehCaracterValido(atual))
+        return montaErro(CARACTERE_INVALIDO, indIn);
+      
+      if(atual == '+' || atual == '-') {
+        indIn++;
+        erro = Termo();
+        
+        if(erro.codigoErro != EXPR_VALIDA)
+          return erro;
+        
+        pos[indPos++] = atual;
+      } else fim = true;
+    } while(!fim);
+ 
+  }
   
-  return resCorreto;  /***** PROVISÓRIO ******/  
+  return resCorreto;
 
 
 } /* Expressao */
 
-
+/*FIXME acertar retornos*/
 Erro Termo() {
 /* Processa um termo da cadeia de entrada.  */
+  Erro erro;
 
-  Fator();
+  erro = Fator();
+  
+  if(erro.codigoErro != EXPR_VALIDA)
+    return erro;
+  
   boolean fim = false;
   do {
     char atual = in[indIn];
-    if(atual == '*' || atual == '/') {
-      indIn++;
-      Fator();
-      pos[indPos++] = atual;
-    } else fim = true;
-    
+    if(!ehCaracterValido(atual))
+      return montaErro(CARACTERE_INVALIDO, indIn);
+    switch(atual) {
+      case '*':
+      case '/': {
+        indIn++;
+        erro = Fator();
+        
+        if(erro.codigoErro != EXPR_VALIDA)
+          return erro;
+        
+        pos[indPos++] = atual;
+      }
+      break;
+      default:
+          fim = true;
+    }
   } while(!fim);
   
   return resCorreto;
 
 } /* Termo */
 
-
+/*FIXME acertar retornos*/
 Erro Fator() {
 /* Processa um fator da cadeia de entrada.  */
-
-  char atual = in[indIn];
-
+  char atual;
+  Erro erro;
+  
+  EliminaBrancos();
+  
+  atual = in[indIn];
+  
+  if(!ehCaracterValido(atual))
+    return montaErro(CARACTERE_INVALIDO, indIn);
+  
   if(atual >= 'a' && atual <= 'z') {
     pos[indPos++] = atual;
     indIn++;
   } else if (atual == '(') {
     indIn++;
-    Expressao();
+    erro = Expressao();
+    
+    if(erro.codigoErro != EXPR_VALIDA)
+      return erro;
+    
     if(in[indIn] == ')')
       indIn++;
     else
       return montaErro(FECHA_PARENTESE_ESPERADO, indIn);
+  } else
+    return montaErro(OPERANDO_ESPERADO, indIn);
+  
+  EliminaBrancos();
+  
+  atual = in[indIn];
+  
+  if(!ehCaracterValido(atual))
+    return montaErro(CARACTERE_INVALIDO, indIn);
+  
+  if (atual == '^') {
+    indIn++;
+    erro = Potencia();
+    
+    if(erro.codigoErro != EXPR_VALIDA)
+      return erro;
+    
+    pos[indPos++] = atual;
   }
   
   return resCorreto;  
 
 } /* Fator */
 
+void EliminaBrancos() {
+  if(in[indIn] == ' ') {
+    indIn++;
+    EliminaBrancos();
+  }
+  
+}
+
+boolean ehCaracterValido(char c) {
+  return (c >= 'a' && c <= 'z') || c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '\0' 
+    || c == ')' || c == '(';
+}
 
 Erro Primario() {
 /* Processa um fator primário da cadeia de entrada.  */
+  char atual;
+  int posAnt;
+  Erro erro;
+
+  atual = in[indIn];
   
+  if(!ehCaracterValido(atual))
+    return montaErro(CARACTERE_INVALIDO, indIn);
+  
+  indIn++;
+  posAnt = indIn;
+  erro = Termo();
+  
+  if(erro.codigoErro != EXPR_VALIDA)
+    return erro;
+    
+  if(indIn == posAnt) {
+    return montaErro(OPERANDO_ESPERADO, indIn);
+  }
 
-
-  /***** COMPLETAR!  *****/
+  pos[indPos++] = atual == '+' ? '&' : '~';
   
   return resCorreto;  /***** PROVISÓRIO ******/  
 
 } /* Primario */
+
+Erro Potencia() {
+/* Processa uma potencia */
+  Erro erro;
+
+  erro = Fator();
+  
+  if(erro.codigoErro != EXPR_VALIDA)
+    return erro;
+  
+  char atual = in[indIn];
+  
+  if(!ehCaracterValido(atual))
+    return montaErro(CARACTERE_INVALIDO, indIn);
+  
+  if(atual == '^') {
+    indIn++;
+    erro = Potencia();
+    
+    if(erro.codigoErro != EXPR_VALIDA)
+      return erro;
+    
+    pos[indPos++] = atual;
+  } 
+
+  return resCorreto;
+
+} /* Potencia */
