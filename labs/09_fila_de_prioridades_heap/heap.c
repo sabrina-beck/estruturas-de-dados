df6528c6aab8d1dd474561ec592e51b37a845e1d
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2014-11-21 17:04:17 -0200

    Lab 09: corrigiu e comentou

--- labs/09_fila_de_prioridades_heap/heap.c
+++ labs/09_fila_de_prioridades_heap/heap.c
@@ -1,7 +1,12 @@
 /*
+ * Nome: Sabrina Beck Angelini      RA: 157240
+ * Turma: E
+ */
+
+/*
   Implementação das funções de manipulação de filas de prioridade
   (FP). Vide o arquivo heap.h com as especificações.
- */  
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,10 +22,14 @@ typedef struct {
 
 
 /* Funções auxiliares para manipulação de FPs. */
+
+/*
+ * Função que sobe um elemento que está fora da regra da FP.
+ */
 void Sobe(ImplHeap h, int m) {
 
   /* Posição do pai do elemento que está subindo */
-  int pai = (m + 1) / 2;
+  int pai = (m - 1) / 2;
   /* Elemento que está subindo na fila de prioridade */
   void *subidor = h->vetor[m];
   
@@ -28,28 +37,61 @@ void Sobe(ImplHeap h, int m) {
    * Enquanto o elemento não chega na raiz e não ocupa um lugar
    * na árvore que atenda as exigência de uma fila de prioridade
    */
-  while((m > 0) &&  h->comp(subidor, h->vetor[pai]) < 0) {
+  while((m > 0) &&  h->comp(subidor, h->vetor[pai]) > 0) {
     h->vetor[m] = h->vetor[pai];
     m = pai;
-    pai = (m + 1) / 2;
+    pai = (m - 1) / 2;
   }
   
+  /*
+   * Por fim coloca o elemento que antes ocupava uma posição errada na FP,
+   * na posição correta, forma otimizada, não precisa trocar a cada descida
+   */
   h->vetor[m] = subidor;  
 } /* Sobe */
 
+/*
+ * Função que desce um elemento que esta forada regra da FP
+ */
 void Desce(ImplHeap h, int m) {
+  /* Posição do filho esquerdo do elemento que desce */
   int filho = 2 * m + 1;
+  /* Valor do elemento que desce */
   void* descedor = h->vetor[m];
   
-  while(m < h->tam) {
-    if(m < (h->tam) - 1 && h->comp(h->vetor[filho], h->vetor[filho + 1]) > 0)
+  /*
+   * Enquanto o elemento que desce não é folha - neste caso está
+   * na posição correta
+   */
+  while(filho < h->tam) {
+    /*
+     * Como a regra dessa FP é que a raiz é menor que seus filhos,
+     * seleciona-se o menor dos filhos para subir na FP e dar lugar
+     * para o elemento que desce
+     */
+    if(filho < ((h->tam) - 1) && h->comp(h->vetor[filho], h->vetor[filho + 1]) < 0)
       filho++;
-
-    h->vetor[m] = h->vetor[filho];
-    m = filho;
-    filho = 2 * m + 1;
+    
+    /*
+     * O elemento deve descer somente se ele filhos menores que ele,
+     * de acordo com a regra dessa FP
+     */
+    if (h->comp(descedor, h->vetor[filho]) < 0) {
+      h->vetor[m] = h->vetor[filho];
+      m = filho;
+      filho = 2 * m + 1;
+    } else
+    /*
+     * Se todos os seus filhos são maiores, então o elemento
+     * já está no lugar certo
+     */
+      break;
   }
   
+  /*
+   * Por fim coloca o elemento que antes ocupava uma posição errada na FP,
+   * na posição correta, forma otimizada, não precisa trocar a cada descida
+   */
   h->vetor[m] = descedor;
 } /* Desce */
 
@@ -59,17 +101,25 @@ Heap CriaHeapAux(int n, funcCompara *comp, void *elems[]) {
      valores do vetor 'elems' e transforma num heap.
   */
   ImplHeap ih = MALLOC(sizeof(RegHeap)+(n-1)*sizeof(void *));
+  /*
+   * Não importa se elems é passado ou não, o tamanho máximo da FP
+   * será n e a função de comparação comp
+   */
   ih->tamMax = n;
   ih->comp = comp;
   
-  if(elems == NULL) {
+  /* Se não há elementos inicialmente na FP, então o tamanho é 0 */
+  if(elems == NULL)
     ih->tam = 0;
-    ih->vetor = MALLOC(sizeof(void*));
-  } else {
+  else {
     int i;
     ih->tam = n;
-    ih->vetor = elems;
+    *(ih->vetor) = *elems;
     
+    /*
+     * Rearranja elems para atender as regras da FP, para isso,
+     * basta subir elemento a elemento do vetor, como se fossem inserções
+     */
     for(i = 1; i < ih->tam; i++)
       Sobe(ih, i);
   }
@@ -78,62 +128,98 @@ Heap CriaHeapAux(int n, funcCompara *comp, void *elems[]) {
   
 } /* CriaHeapAux */
 
+/*
+ * Cria uma FP vazia com espaço de até 'n' elementos que serão
+ * apontadores; 'comp' será a função de comparação para estes elementos.
+ */
 Heap CriaHeap(int n, funcCompara *comp) {
 
   return CriaHeapAux(n,comp,NULL);
-
+  
 } /* CriaHeap */
-
+/* Libera toda a memória dinâmica ocupada pela FP 'h', mas não por
+   seus elementos.
+*/
+/*
+ * Cria uma FP de 'n' elementos apontadores fornecidos no vetor 'elems'.
+ * 'comp' será a função de comparação para estes elementos.
+ */
 Heap CriaInicializaHeap(int n, funcCompara *comp, void *elems[]) {
 
-
   return CriaHeapAux(n,comp,elems);
-
+  
 } /* CriaInicializaHeap */
 
+/* Devolve o número de elementos contidos na FP. */
 int TamanhoHeap(Heap h) {
-  
-  return h.tam;
+  /* Para isso, é preciso usar a implementação de Heap: ImplHeap */  
+  return ((ImplHeap) h)->tam;
   
 } /* TamanhoHeap */
 
+/*
+ * Insere o elemento 'e' na FP 'h'. Interrompe a execução do programa
+ * se houver estouro da FP.
+ */
 void InsereHeap(Heap h, void *e) {
 
   ImplHeap ih = h;
+  /*
+   * Se não há mais espaço para elementos na FP, o programa não
+   * pode continuar
+   */
   if (ih->tam==ih->tamMax) {
     printf("Estouro da FP\n");
     exit(0);
   }
 
-  h->vetor[h->tam] = e;
-  (h->tam)++;
-  Sobe(h, (h->tam) - 1);
+  /*
+   * Na inserção, basta colocar o novo elemento no final
+   * da fila de prioridade e subí-lo até sua posição correta
+   */
+  ih->vetor[ih->tam] = e;
+  (ih->tam)++;
+  Sobe(ih, (ih->tam) - 1);
 } /* insereHeap */
 
-
+/*
+ * Remove e devolve o elemento máximo (de acordo com a função 'comp')
+ * da FP.  Interrompe a execução do programa se a FP está vazia.
+ */
 void * RemoveHeap(Heap h) {
 
   ImplHeap ih = h;
   void *ret = NULL;
+  /*
+   * Se não há mais espaço para elementos na FP, o programa não
+   * pode continuar
+   */
   if (ih->tam==0) {
     printf("FP vazia\n");
     exit(0);
   }
 
-  ret = h->vetor[0];
-  (h->tam)--;
-  h->vetor[0] = h->vetor[h->tam];
-  Desce(h, 0);
+  /*
+   * Na remoção, basta trocar o primeiro com o último elemento
+   * da fila de prioridade e descê-lo até sua posição correta
+   * Note que como é uma fila, o primeiro elemento é o removido
+   * logo a raiz é removida.
+   */
+  ret = ih->vetor[0];
+  (ih->tam)--;
+  ih->vetor[0] = ih->vetor[ih->tam];
+  Desce(ih, 0);
 
   return ret;
   
 } /* removeHeap */
 
+/*
+ * Libera toda a memória dinâmica ocupada pela FP 'h', mas não por
+ * seus elementos.
+ */
 void LiberaHeap(Heap h) {
-
-  FREE(h->vetor);
   FREE(h);
-
 } /* liberaHeap */
 
 
