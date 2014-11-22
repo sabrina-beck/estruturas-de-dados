d8c74e245eb8678f39c5ac6e8e3f5b93d629a766
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2014-11-21 23:21:46 -0200

    Adicionou labs 11 e 12

---
+++ labs/11_base_de_dados_com_espalhamento_encadeado/base.c
@@ -0,0 +1,46 @@
+/*
+  Autor:         COMPLETAR!
+  RA:            COMPLETAR!
+  Disciplina:    MC202
+  Turmas:        E e F
+  
+  Tarefa 11
+  Segundo semestre de 2014
+
+*/
+
+#include <stdio.h>
+#include "base.h"
+
+/* DECLARAÇÕES DE TIPOS */
+/* -------------------- */
+
+typedef struct RegLista *Lista;
+typedef struct RegLista {
+  Aluno aluno;
+  Lista prox;
+} RegLista;
+
+typedef struct RegBase *ImplBase;
+typedef struct RegBase {
+  int numregs;
+  Lista tabela[MaxHash];
+} RegBase;
+
+/* FUNÇÃO DE ESPALHAMENTO */
+/* ---------------------- */
+
+int Espalha(int k) {
+  return (k%MaxHash);
+} /* Espalha */
+
+/* FUNÇÕES AUXILIARES */
+/* ------------------ */
+
+/* COMPLETAR SE NECESSÁRIO */
+
+
+/* FUNÇÕES DA INTERFACE */
+/* -------------------- */
+
+/* COMPLETAR */
