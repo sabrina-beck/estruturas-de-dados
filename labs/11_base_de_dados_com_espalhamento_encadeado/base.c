7ebd2303c19e6732b4937323396d1bdf905d852f
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2014-11-23 14:54:15 -0200

    Lab 11: compilou

--- labs/11_base_de_dados_com_espalhamento_encadeado/base.c
+++ labs/11_base_de_dados_com_espalhamento_encadeado/base.c
@@ -1,8 +1,8 @@
 /*
-  Autor:         COMPLETAR!
-  RA:            COMPLETAR!
+  Autor:         Sabrina Beck Angelini
+  RA:            157240
   Disciplina:    MC202
-  Turmas:        E e F
+  Turma:         E
   
   Tarefa 11
   Segundo semestre de 2014
@@ -31,7 +31,7 @@ typedef struct RegBase {
 /* ---------------------- */
 
 int Espalha(int k) {
-  return (k%MaxHash);
+  return (k % MaxHash);
 } /* Espalha */
 
 /* FUNÇÕES AUXILIARES */
@@ -43,4 +43,65 @@ int Espalha(int k) {
 /* FUNÇÕES DA INTERFACE */
 /* -------------------- */
 
-/* COMPLETAR */
+/*
+ * Devolve apontador para uma base vazia com tabela inicializada
+ */
+Base CriaBase() {
+  /*TODO*/
+  return NULL;
+}
+
+/*
+ * Insere o registro 'a' na base 'p' se não existe aluno
+ * com o mesmo valor de 'ra', e devolve 'true';  caso
+ * contrário devolve 'false'
+ */
+Boolean InsereBase(Base p, Aluno *a) {
+  /*TODO*/
+  return false;
+}
+   
+/*
+ * Remove da base 'p' o aluno com 'ra' dado; devolve 'true'
+ * se ele existiu e 'false' caso contrário.
+ */
+Boolean RemoveBase(Base p, int ra) {
+  /*TODO*/
+  return false;
+}
+
+/* Devolve 'true' se existe um registro com 'ra' dado na
+ * base 'p';  caso contrário devolve 'false'.  'a' conterá 
+ * os dados do aluno, se encontrado.
+ */
+Boolean ConsultaBase(Base p, int ra, Aluno *a) {
+  /*TODO*/
+  return false;
+}
+   
+/*
+ * Devolve o número de registros (alunos) na base 'p'.
+ */
+int NumeroRegsBase(Base p) {
+  /*TODO*/
+  return 0;
+}
+
+/* Imprime, os registros contidos na base 'p', um por linha.  A ordem
+ * de impressão segue a ordem das entradas da tabela, e para cada
+ * entrada, a ordem da lista ligada.  Cada linha começa com o índice
+ * correspondente na tabela de espalhamento. Deve ser usado o formato
+ * "(%3d) %06d %s\n".
+ */
+void ImprimeBase(Base p) {
+  /*TODO*/
+}
+   
+/*
+ * Libera toda a memória utilizada pela base 'p', bem como
+ * todas as cadeias que guardam os nomes.
+ */
+void LiberaBase(Base p) {
+  /*TODO*/
+}
+
